6dee7faca06e58127cdd3ba31438a71a0703402a
Eugen Diachenko <eugen.diachenko@levels.one>
2018-06-27 14:16:08 +0200

    Framework project added

---
+++ GLNotificationBar.h
@@ -0,0 +1,19 @@
+//
+//  GLNotificationBar.h
+//  GLNotificationBar
+//
+//  Created by Eugen Diachenko on 27.06.18.
+//  Copyright © 2018 Gokul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GLNotificationBar.
+FOUNDATION_EXPORT double GLNotificationBarVersionNumber;
+
+//! Project version string for GLNotificationBar.
+FOUNDATION_EXPORT const unsigned char GLNotificationBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GLNotificationBar/PublicHeader.h>
+
+
